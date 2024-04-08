31db7d3654fdfc72d36b11661a8ae34e223a6820
James Donckels <jdonckels1@go.pasadena.edu>
2024-04-08 15:31:41 -0700

    test project success

---
+++ build/CMakeFiles/MazeGenerator.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MazeGenerator.dir/main.cpp.o"
+  "CMakeFiles/MazeGenerator.dir/main.cpp.o.d"
+  "MazeGenerator"
+  "MazeGenerator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MazeGenerator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
